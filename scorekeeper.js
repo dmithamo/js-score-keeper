31834fb79b3951413a8a0632065132884044f0c1
dbdennot8 <dennisbmithamo@gmail.com>
2017-12-13 17:38:47 +0300

    Set app to work only if winning score is set

--- scorekeeper.js
+++ scorekeeper.js
@@ -8,7 +8,6 @@ var maxScore = document.getElementById("set-max");
 var maxScoreDisplay = document.querySelector("#max-line");
 var gameOver = false;
 
-
 function defineMax(){
     maxScoreDisplay.textContent = maxScore.value;
 }
@@ -19,22 +18,30 @@ function markWinner(){
 
 function addScorePlayer1(){
     if(!gameOver){
-        player1Score ++;
-        player1ScoreDisplay.textContent = player1Score;
-        if(player1Score === +maxScore.value){
-            player1ScoreDisplay.classList.add("mark-winner")
-            gameOver = true;
+        if(+maxScore.value === 0){
+            alert("Winning score not set")
+        } else{
+            player1Score ++;
+            player1ScoreDisplay.textContent = player1Score;
+            if(player1Score === +maxScore.value){
+                player1ScoreDisplay.classList.add("mark-winner")
+                gameOver = true;
+            }
         }
     }
 }
           
 function addScorePlayer2(){
     if(!gameOver){
-        player2Score ++;
-        player2ScoreDisplay.textContent = player2Score;
-        if(player2Score === +maxScore.value){
-            player2ScoreDisplay.classList.add("mark-winner")
-            gameOver = true;
+        if(+maxScore.value === 0){
+            alert("Winning score not set")
+        } else{
+            player2Score ++;
+            player2ScoreDisplay.textContent = player2Score;
+            if(player2Score === +maxScore.value){
+                player2ScoreDisplay.classList.add("mark-winner")
+                gameOver = true;
+            }
         }
     }
 }
